f2828fa7b170ad18aa88e24cd002d8c0f7e4a1fe
priya <priyadontha@gmail.com>
2020-05-16 01:58:41 -0700

    Updates

---
+++ precache-manifest.10393ec2a480f74a012746c7ea47c54f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3ad5f2dfff1741caa365c4dd61d1a82",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "52639f30d373ac91df40",
+    "url": "/monsters-rolodex/static/css/main.73a2fa78.chunk.css"
+  },
+  {
+    "revision": "1d6d326f4470ed67fe85",
+    "url": "/monsters-rolodex/static/js/2.46e2921d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.46e2921d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "52639f30d373ac91df40",
+    "url": "/monsters-rolodex/static/js/main.2e2162bd.chunk.js"
+  },
+  {
+    "revision": "aa1aeb4ed7e62e71a38e",
+    "url": "/monsters-rolodex/static/js/runtime-main.2292ee2f.js"
+  }
+]);
